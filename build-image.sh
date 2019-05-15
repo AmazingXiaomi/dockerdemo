c09d51285f9f2a16946d88c931e8831cedc98e1a
yinxiaolai <1205938863@qq.com>
2019-05-15 17:34:46 +0800

    init

---
+++ build-image.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    a1205938863@gmail.com                          $'
+echo '$  homePage: https://xiaomiller.cn                          $'
+echo '$  Github:   https://github.com/AmazingXiaomi               $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+# if you have engine project . plz add
+# cd ${your engine path}
+# mvn clean install
+
+cd cloudclient
+
+mvn clean package docker:build
+
+cd ..
+
+cd cloudserver
+
+mvn clean package docker:build
+
+cd ..
+
+cd eureka-server
+
+mvn clean package docker:build
+
+cd ..
+
+cd ..
+
+
---
+++ rm-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    a1205938863@gmail.com                          $'
+echo '$  homePage: https://xiaomiller.cn                          $'
+echo '$  Github:   https://github.com/AmazingXiaomi               $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker rmi xiaomiller/docker-compose-config:v1.0.0
+docker rmi xiaomiller/docker-compose-eureka:v1.0.0
+docker rmi xiaomiller/docker-compose-gateway:v1.0.0
